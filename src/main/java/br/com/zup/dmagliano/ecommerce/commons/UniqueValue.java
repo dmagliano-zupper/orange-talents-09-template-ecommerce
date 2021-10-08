8ff30eb9605e53ba08bebc1a8510f15ae0b19d45
Diogo Magliano <diogo.magliano@zup.com.br>
2021-10-08 14:53:41 -0300

    Implementação de validador para dados unicos.

---
+++ src/main/java/br/com/zup/dmagliano/ecommerce/commons/UniqueValue.java
@@ -0,0 +1,27 @@
+package br.com.zup.dmagliano.ecommerce.commons;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target({ElementType.FIELD})
+@Constraint(validatedBy = {UniqueValueValidator.class})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UniqueValue {
+
+    String message() default "O valor do campo já foi cadastrado!";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+
+    String fieldName();
+
+    Class<?> domainClass();
+
+}
---
+++ src/main/java/br/com/zup/dmagliano/ecommerce/commons/UniqueValueValidator.java
@@ -0,0 +1,31 @@
+package br.com.zup.dmagliano.ecommerce.commons;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+public class UniqueValueValidator implements ConstraintValidator<UniqueValue, Object> {
+
+    private String domainAttribute;
+    private Class<?> klass;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void initialize(UniqueValue params) {
+        domainAttribute = params.fieldName();
+        klass = params.domainClass();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext constraintValidatorContext) {
+        Query query = em.createQuery("SELECT 1 FROM " + klass.getName() + " WHERE " + domainAttribute + " = :value");
+        query.setParameter("value", value);
+        List<?> list = query.getResultList();
+        return list.isEmpty();
+    }
+}
